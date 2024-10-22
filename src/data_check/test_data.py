35eab3ee1e46bc9f832e185677893a5fb0f23d35
Zhixiang <zhixianglou@gmail.com>
2024-10-22 14:03:31 -0500

    Updated conda.yml to resolve pytest and scipy version compatibility for Python 3.10

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,12 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+
+def test_row_count(data):
+    # Check that the size of the dataset is reasonable
+    assert 15000 < data.shape[0] < 1000000, f"Row count is {data.shape[0]}, which is out of the expected range."
+
+def test_price_range(data, min_price, max_price):
+    # Check that the price range is between min_price and max_price
+    assert data['price'].between(min_price, max_price).all(), f"Prices are not within the range {min_price} - {max_price}."
